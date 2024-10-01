548b6b6550bf05fcd3af48b6d39bc0de8d5f6233
izzetcosar <izzetcosar@gmail.com>
2024-10-01 16:23:02 +0300

    Add files via upload

---
+++ izzetcosar/helloworld/helloworld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Helloworld");
+
+
+        }
+
+    }
+
+}
---
+++ izzetcosar/ilkproje/ilkproje/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ilk_uygulamam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("izzet cosar  ");
+            Console.WriteLine("Patikanın uzman kadrosu ve tecrübeli egitmenleri sayesinde yazılım dünyasına hızlı bir sekilde giris yapabilecegimi için bu bootcampdeyim!");
+        }
+
+    }
+}
